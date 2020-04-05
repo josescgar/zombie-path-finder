3383087c078953bdb58e4e9ae3aa6171769fdc48
Jose Escobar <jose.escobar@gig.com>
2020-04-05 12:17:48 +0200

    Updates

---
+++ precache-manifest.28248809d77d5292389e8a3872eea6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873ca3374cf5318d26f6a898d310df02",
+    "url": "/zombie-path-finder/index.html"
+  },
+  {
+    "revision": "0eca1f408e0d04569478",
+    "url": "/zombie-path-finder/static/css/main.d3d1694b.chunk.css"
+  },
+  {
+    "revision": "986ebb4f6ad7d2d0b144",
+    "url": "/zombie-path-finder/static/js/2.5ebd3964.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/zombie-path-finder/static/js/2.5ebd3964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eca1f408e0d04569478",
+    "url": "/zombie-path-finder/static/js/main.e414759d.chunk.js"
+  },
+  {
+    "revision": "537bc9b526db64b46637",
+    "url": "/zombie-path-finder/static/js/runtime-main.3e243138.js"
+  }
+]);
